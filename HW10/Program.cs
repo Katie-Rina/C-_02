8fb2eba04fcfee720e0158979ad889af28487dea
KatieRina <eashchukina@yandex.ru>
2022-10-18 22:46:58 +0300

    первая задача ДЗ

--- HW10/Program.cs
+++ HW10/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = 0;
+if (Math.Abs(number) > 999 | Math.Abs(number) < 100)
+{
+    Console.WriteLine("Число не трёхзначное");
+}
+else
+{
+    a = Math.Abs(number) % 100 /10;
+    Console.WriteLine("Вторая цифра: " + a);
+}
+
