6510c773667087f0d3a09bc0be02fb18827dbcf8
KatieRina <eashchukina@yandex.ru>
2022-10-18 23:16:15 +0300

    вторая задача ДЗ

--- HW13/Program.cs
+++ HW13/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int a = 0;
+// if (Math.Abs(number) < 100)
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+// else
+// {
+//     a = Math.Abs(number);
+//     string num = a.ToString();
+//     Console.WriteLine("Третья цифра: " + num[2]);
+// }
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+char[] minus = {'-'};
+string absnumber = number.TrimStart(minus);
+if (absnumber.Length < 3)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine("Третья цифра: " + absnumber[2]);
+}
