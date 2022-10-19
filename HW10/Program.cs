0e9bee922d315ed3367ba27096ca17723b75559a
KatieRina <eashchukina@yandex.ru>
2022-10-19 21:59:32 +0300

    первая задача 2 вариант

--- HW10/Program.cs
+++ HW10/Program.cs
@@ -1,16 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+﻿// Console.Clear();
+// Console.Write("Введите трёхзначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int a = 0;
+// if (Math.Abs(number) > 999 | Math.Abs(number) < 100)
+// {
+//     Console.WriteLine("Число не трёхзначное");
+// }
+// else
+// {
+//     a = Math.Abs(number) % 100 /10;
+//     Console.WriteLine("Вторая цифра: " + a);
+// }
+
+
 Console.Clear();
-Console.Write("Введите трёхзначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int a = 0;
-if (Math.Abs(number) > 999 | Math.Abs(number) < 100)
+Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+char[] minus = {'-','0'};
+string absnumber = number.TrimStart(minus);
+if (absnumber.Length != 3)
 {
     Console.WriteLine("Число не трёхзначное");
 }
 else
 {
-    a = Math.Abs(number) % 100 /10;
-    Console.WriteLine("Вторая цифра: " + a);
+    Console.WriteLine("Вторая цифра: " + absnumber[1]);
 }
-
