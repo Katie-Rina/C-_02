dd1b084f62fd4998989dc2cb1a82e174bb824ddc
KatieRina <eashchukina@yandex.ru>
2022-10-19 22:16:22 +0300

    третья задача

--- HW15/Program.cs
+++ HW15/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+Console.Clear();
+Console.Write("Введите номер дня недели от 1 до 7: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day < 1 | day > 7)
+{
+    Console.WriteLine("Это не номер дня недели");
+}
+else 
+{
+    if (day > 5)
+    {
+        Console.WriteLine("Выходной");
+    }
+    else
+    {
+        Console.WriteLine("Рабочий");
+    }
+}
